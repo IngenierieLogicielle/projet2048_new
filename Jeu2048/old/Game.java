4ed80b9e8f39db3513e139fdf450566767071867
JDzim <joseph.dzimbalka@gmail.com>
2015-04-06 23:37:26 +0200

    Ajout des fichiers de l'étape 1

--- Jeu2048/old/Game.java
+++ Jeu2048/old/Game.java
@@ -1,11 +1,70 @@
 package old;
 
+import java.util.Scanner;
+
 public class Game
 {
 	public static void main(String args[])
 	{
-		// Modif Joseph
-            // modif Joan 2
-            // Pour mettre en ligne, d'abord faire un commit puis un push et pour récupérer faire pull. 
+		// System.out.println('A');
+		
+		boolean end = false;
+		String saisie;
+		Scanner s = new Scanner(System.in);
+		Grid grid = new Grid(4,4); // System.out.println('A');
+		grid.initGrid(); // System.out.println('A');
+		grid.displayGrid();
+		
+		// System.out.println('B');
+		
+		while (!end)
+		{
+			System.out.println("Prochain deplacement : ");
+			System.out.println("2 - bas");
+			System.out.println("8 - haut");
+			System.out.println("4 - gauche");
+			System.out.println("6 - droite");
+			System.out.println("Autres commandes :");
+			System.out.println("5 - nouvelle grille");
+			System.out.println("0 - fin");
+
+			saisie = s.nextLine();
+			
+			switch(saisie)
+			{
+				case "2":
+					grid.moveDown();
+					grid.addCase();
+					grid.displayGrid();
+					break;
+				case "8":
+					grid.moveUp();
+					grid.addCase();
+					grid.displayGrid();
+					break;
+				case "4":
+					grid.moveLeft();
+					grid.addCase();
+					grid.displayGrid();
+					break;
+				case "6":
+					grid.moveRight();
+					grid.addCase();
+					grid.displayGrid();
+					break;
+				case "5":
+					grid.initGrid();
+					grid.displayGrid();
+					break;
+				case "0":
+					end = true;
+					break;
+				default:
+					end = true;
+					break;
+			}
+		}
+		
+		// System.out.println(Grid.customRandom(4));
 	}
 }
