4eabec70b79e044f56153ce0f04200d3f706a6d3
JDzim <joseph.dzimbalka@gmail.com>
2015-04-06 23:28:14 +0200

    Ajout des fichiers de l'étape 1

---
+++ Jeu2048/src/old/Case.java
@@ -0,0 +1,54 @@
+package old;
+import java.lang.Math.*;
+
+public class Case
+{
+    private int x;
+    private int y;
+    private int value;
+  
+    
+    public Case(){
+        this.x = x;
+        this.y = y;
+        this.value = value;
+    }
+    
+    // modif Joseph
+    
+    // getters
+    public int getX(){
+        return this.x;
+    }
+     public int getY(){
+        return this.y;
+    }
+      public int getValue(){
+        return this.value;
+    }
+      
+    //setters
+      
+    public void setX(int x){
+        this.x = x;
+    }
+    
+    public void setY(int y){
+        this.y = y;
+    }
+    
+    public void setValue(int value){
+        this.value = value;
+    }
+      
+    //détection de la case suivante
+    public boolean getNextCase(int direction){
+        
+        return true;      
+    }
+    //fonction pour le mouvement 
+    public boolean move5LeRetourDuRetour (Case c){
+        
+        return true;    
+    }
+}
