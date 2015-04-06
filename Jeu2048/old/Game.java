c2d0b84226cbc07febe83c43faaeaa3abfceb31e
JDzim <joseph.dzimbalka@gmail.com>
2015-04-06 23:28:26 +0200

    Ajout des fichiers de l'étape 1

---
+++ Jeu2048/old/Game.java
@@ -0,0 +1,11 @@
+package old;
+
+public class Game
+{
+	public static void main(String args[])
+	{
+		// Modif Joseph
+            // modif Joan 2
+            // Pour mettre en ligne, d'abord faire un commit puis un push et pour récupérer faire pull. 
+	}
+}
---
+++ Jeu2048/old/Grid.java
@@ -0,0 +1,14 @@
+package old;
+import java.lang.Math.*;
+
+public class Grid
+{
+	private int height;
+	private int width;
+	private Case[][] cases;
+	
+	public Grid()
+	{
+            System.out.println("testgit");
+        }
+}
