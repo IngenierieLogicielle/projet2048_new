a668ca71f5cbf54b5e0e2deaf98baa67e56d73c9
Somnir <matthieu.burtin.somnir@gmail.com>
2015-04-30 22:00:10 +0200

    Test debug javafx

--- Jeu2048/src/jeu2048/Jeu2048.java
+++ Jeu2048/src/jeu2048/Jeu2048.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jeu2048;
+
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -34,7 +30,9 @@ public class Jeu2048 extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+        
     }
+    
     
 }
 
