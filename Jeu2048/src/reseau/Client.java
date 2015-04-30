e86070cb61532dd9a681f1ee9282e3e1dbf74553
JDzim <joseph.dzimbalka@gmail.com>
2015-04-30 21:03:42 +0200

    Création des classes réseau

---
+++ Jeu2048/src/reseau/Client.java
@@ -0,0 +1,12 @@
+package reseau;
+
+/**
+ * Classe Grid,
+ * Représente le côté client (ordinateur du joueur).
+ * 
+ * @author Joan BELO, Matthieu BURTIN, Joseph DZIMBALKA
+ */
+public class Client 
+{
+    
+}
---
+++ Jeu2048/src/reseau/Serveur.java
@@ -0,0 +1,12 @@
+package reseau;
+
+/**
+ * Classe Serveur,
+ * Représente le côté serveur (serveur application et base de données).
+ * 
+ * @author Joan BELO, Matthieu BURTIN, Joseph DZIMBALKA
+ */
+public class Serveur 
+{
+    
+}
